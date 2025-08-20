39cd91e74ba91c49c0c60f8a304bb8ae86d47b23
avinash singh patel <130283633+algo-avi@users.noreply.github.com>
2025-08-20 11:12:00 +0530

    allowing image domain example.com

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ['img.freepik.com', "previews.123rf.com"],
+      domains: ['img.freepik.com', "previews.123rf.com","example.com"],
     },
 };
 
