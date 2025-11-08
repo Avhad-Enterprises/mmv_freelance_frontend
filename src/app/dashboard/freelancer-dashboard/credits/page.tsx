8a6a5be9627450cf17a23d5a3c3ee3b4c51650f1
atupatu <atharva.patil22@spit.ac.in>
2025-11-09 05:06:53 +0530

    Credits

---
+++ src/app/dashboard/freelancer-dashboard/credits/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react';
+import CreditsArea from '@/app/components/dashboard/candidate/credits';
+
+const CandidateDashboardPage = () => {
+  return <CreditsArea />;
+};
+
+export default CandidateDashboardPage;
